72565fbd04f59f6b23764b5cfff8e8a3509f2bf0
colin <clawrence005@yahoo.com>
2019-03-23 16:09:54 -0500

    basic structure complete. friend algorithm doesnt work yet.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,7 @@
 
 // Your apiRoutes.js file should contain two routes:
-
-
-
 // ===============================================================================
 var friends = require("../data/friends");
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -24,34 +20,10 @@ module.exports = function (app) {
 
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-    app.post("/api/survey", function (req, res) {
-        if (tableData.length < 5) {
-            newF.push(req.body);
-            res.json(friends);
-        });
-
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
-    // app.post("/api/friends", function (req, res) {
-    //     Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    //     It will do this by sending out the value "true" have a table
-    //     req.body is available since we're using the body parsing middleware
-
-    //     if (friends.length < 5) {
-    //       tableData.push(req.body);
-    //       res.json(true);
-    //     }
-    //     else {
-    //       waitListData.push(req.body);
-    //       res.json(false);
-    //     }
-    // });
+    app.post("/api/friends", function (req, res) {
+        friends.push(req.body);
+        res.json(friends);
+    });
 
     // ---------------------------------------------------------------------------
     // I added this below code so you could clear out the table while working with the functionality.
