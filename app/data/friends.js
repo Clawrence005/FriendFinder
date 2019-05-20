056a6b1c73ded62e9ca588bf7d21fcf033c2743f
colin <clawrence005@yahoo.com>
2019-05-20 17:47:32 -0500

    algorithm has a bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,78 +1,7 @@
-
-// var arr1 = [5, 1, 3, 4, 4, 1, 3, 5, 4, 3];
-// var arr2 = [1, 5, 3, 4, 2, 1, 3, 5, 1, 3];
-// //function that consoles the friends var scores
-// var friendsToCompare = function () {
-//     for (i = 0; i < friends.length; i++) {
-//         var info = console.log(friends[i].scores)
-//     } return info;
-// }
-// friendsToCompare();
-
-// //function using two arrays passed in that makes new array with the differences between arrays
-// function diffArr(arr1, arr2) {
-//     var newArr = [];
-//     for (i = 0; i < arr1.length; i++) {
-//         var absoluteResult = Math.abs(arr1[i] - arr2[i]);
-//         // compare indexof arr1 to arr2
-//         newArr.push(absoluteResult);
-//     }
-//     return newArr;
-// }
-
-
-// //function using two arrays passed in that shows number difference between arrays
-// function diffNum(arr1, arr2) {
-//     var totalDiff = 0;
-//     for (i = 0; i < arr1.length; i++) {
-//         var absoluteResult = Math.abs(arr1[i] - arr2[i]);
-//         totalDiff += absoluteResult;
-//     }
-//     return totalDiff;
-// }
-// console.log("diffArr(arr1, arr2): " + diffArr(arr1, arr2));
-// console.log("diffNum(arr1, arr2): " + diffNum(arr1, arr2));
-
-
-
-// module.exports = diffArr(arr1, arr2);
-// module.exports = diffNum(arr1, arr2);
-
-// `
-// im not sure how to iterate through the below scores and check which matches the users
-// console logs:
-// :FriendFinder create$ node algorerhythm.js 
-// [ 5, 1, 4, 4, 3, 1, 2, 5, 4, 1 ]
-// [ 5, 1, 4, 1, 5, 3, 1, 5, 4, 1 ]
-// [ 5, 1, 4, 4, 4, 1, 3, 2, 4, 1 ]
-// [ 5, 1, 4, 1, 5, 1, 3, 1, 4, 5 ]
-// [ 5, 1, 4, 4, 5, 1, 1, 5, 4, 1 ]
-// [ 5, 1, 3, 4, 4, 1, 3, 5, 4, 3 ]
-// diffArr(arr1, arr2): 4,4,0,0,2,0,0,0,3,0
-// diffNum(arr1, arr2): 13 
-// `
-
-// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
-
-
-// Determine the user's most compatible friend using the following as a guide:
-
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-// Example:
-// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-// Total Difference: 2 + 1 + 2 = 5
-// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-// The closest match will be the user with the least amount of difference.
-// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-
-// The modal should display both the name and picture of the closest match.
-
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Jenna Maroney",
+        "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-01/12/20/campaign_images/webdr14/jenna-maroney-30-rock-2-31231-1452647315-5_dblbig.jpg",
         "scores": [
             5,
             1,
@@ -88,7 +17,7 @@ var friends = [
     },
     {
         "name": "CookieMonster",
-        "photo": "",
+        "photo": "https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F930423%252F97a69861-1354-408b-9551-e24803ef956f.jpg%252F950x534__filters%253Aquality%252890%2529.jpg?signature=jkkQCcV7Vj2Piswya7iuKBckEpc=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com",
         "scores": [
             5,
             1,
